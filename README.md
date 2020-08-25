731a0059821a27395f4c38843ec6b94b21e2e5c1
netbeifeng <ekuralc@gmail.com>
2020-08-26 00:01:53 +0200

    first push

--- README.md
+++ README.md
@@ -1,1 +1,81 @@
 # hexo-musicxml-sheetmusic
+
+ ![NPM VERSION](https://img.shields.io/npm/v/hexo-musicxml-sheetmusic.svg) ![LANGUAGE](https://img.shields.io/badge/language-javascript-chocolate.svg) ![](https://img.shields.io/badge/license-MIT-deepskyblue.svg)
+
+Embed a **sheet music** form a **local / online musicXML** file on your [Hexo](https://hexo.io/) article, used svg-render from [osmd](https://github.com/opensheetmusicdisplay/opensheetmusicdisplay). According to different needs, you can choose whether to load the player.
+
+读取musicXML文件，并在 hexo 文章中显示。使用 osmd 的渲染引擎。根据需求可以设置是否加载播放器。
+
+Demo Musicxml: [A little white boat / 小白船](https://cdn.jsdelivr.net/npm/hexo-musicxml-sheetmusic@0.0.5/A_little_white_boat.musicxml)
+
+More Musicxml here: [Musicxml Example-Set](https://www.musicxml.com/music-in-musicxml/example-set/)
+
+## Demo without Player / 不带播放器的实例
+
+![](https://s1.ax1x.com/2020/08/26/d22BwR.png)
+
+## Demo with Player / 带播放器的实例
+
+![](https://s1.ax1x.com/2020/08/26/d22dOJ.png)
+
+## Installation / 安装
+![Install](https://nodei.co/npm/hexo-musicxml-sheetmusic.png?downloads=true&downloadRank=true&stars=true)
+
+```bash
+npm install hexo-musicxml-sheetmusic
+```
+
+## Usage / 使用方法
+
+Use tags in Markdown:
+
+在 Markdown 文件中：
+
+### Without Player / 无播放器
+```
+{% musicxml URL "false" %}
+```
+
+### With Player / 有播放器
+```
+{% musicxml URL "true" %}
+```
+
+#### Local File Example / 本地文件:
+
+```
+{% musicxml C:\Flute\moon.musicxml "false" %}
+```
+
+#### Internet File Example / 网络文件:
+
+```
+{% musicxml https://wpmedia.musicxml.com/wp-content/uploads/2017/12/Echigo-Jishi.musicxml "false" %}
+```
+
+## Attention / 注意
+
+该插件正处在开发阶段，各项功能均不稳定。 
+
+The plug-in is in the development stage and all functions are unstable. You have been warned.
+
+已知的 Bug 包括: / Known Bugs include:
+- 多行乐谱时，点击音符跳转的位置错误 / When there are multiple lines of scores, the clicked notes jump to the wrong position
+- 导出 MIDI 的 MIDI 乐器只有 0 号钢琴 / export MIDI only avaliable with Midi Instrument 0 piano 
+- 导出 PDF 不能正常分页，故不能正常打印 / Exported PDF cannot be paged normally, so it cannot be printed normally
+
+## Dependencies / 依赖
+依赖如下: / Dependencies as follows:
+- [OpenSheetMusicDisplay](https://github.com/opensheetmusicdisplay/opensheetmusicdisplay)
+- [OSMD Audio Player](https://github.com/jimutt/osmd-audio-player)
+- [FileSaver.js](https://github.com/eligrey/FileSaver.js/)
+- [minify-xml](https://www.npmjs.com/package/minify-xml)
+- [pdfKit](https://github.com/foliojs/pdfkit)
+- [Blob-Stream](https://github.com/devongovett/blob-stream)
+- [SVG-to-PDFKit](https://github.com/alafr/SVG-to-PDFKit)
+- [GopherJS](https://github.com/gopherjs/gopherjs)
+- [jQuery](https://github.com/jquery/jquery)
+
+## License
+
+MIT
---
+++ index.js
@@ -0,0 +1,134 @@
+const util = require('hexo-util');
+const request = require('request-promise');
+const minifyXML = require("minify-xml").minify;
+const fs = require("fs");
+const cdn = 'https://cdn.jsdelivr.net/npm/opensheetmusicdisplay@0.8.3/build/opensheetmusicdisplay.min.js';
+
+hexo.extend.tag.register('musicxml', function (args) {   
+    var musicxml = args[0];
+    var loadPlayer = args[1];
+    // console.log(loadPlayer);
+
+    if(loadPlayer === "true") {
+        console.log("Load With Player!");
+        var css = util.htmlTag('link', {
+            href: 'https://cdn.jsdelivr.net/npm/hexo-musicxml-sheetmusic@0.0.5/osmd-c.css',
+            rel: 'stylesheet'
+        });
+
+        var loding_box = util.htmlTag('div', {
+            class: "boxLoading",
+            id: "loading"
+        },"<label style='position: relative;right: 25%;top: 150%;'>Loading...</label>",false);
+    
+        var pdfKit_js = util.htmlTag('script',{
+            src: 'https://cdn.jsdelivr.net/npm/hexo-musicxml-sheetmusic@0.0.5/pdfkit.js'
+        },"",false);
+    
+        var blobStream_js = util.htmlTag('script',{
+            src: 'https://cdn.jsdelivr.net/npm/hexo-musicxml-sheetmusic@0.0.5/blobstream.js'
+        },"",false);
+    
+        var svg_pdfKit_js = util.htmlTag('script',{
+            src: 'https://cdn.jsdelivr.net/npm/svg-to-pdfkit@0.1.8/source.js'
+        },"",false);
+    
+        var mxl2midi_js = util.htmlTag('script', {
+            src: "https://cdn.jsdelivr.net/npm/hexo-musicxml-sheetmusic@0.0.5/gjs.js"
+        },"",false);
+    
+        var osmd_app_js = util.htmlTag('script',{
+            src: 'https://cdn.jsdelivr.net/npm/hexo-musicxml-sheetmusic@0.0.5/osmd-app.min.js'
+        }, "",false);
+    
+        var jquery_js = util.htmlTag('script',{
+            src: 'https://cdn.jsdelivr.net/npm/jquery@3.2.1/dist/jquery.min.js'
+        }, "",false); 
+    
+        var _js_bundle =  jquery_js + pdfKit_js + mxl2midi_js + blobStream_js + svg_pdfKit_js;
+    
+        var _canvas = util.htmlTag('canvas', {id: "canvas", style: "display:none;"},"",false);
+
+        if(musicxml.indexOf("http") != -1) {
+            return result = getMusicXML(musicxml).then(text => {
+    
+                var script = util.htmlTag('script', {}, 'var mxl = \'' + text + '\';', false);
+                
+                var osmdCanvas = util.htmlTag('div', {
+                    id: 'osmdCanvas'
+                }, loding_box + _canvas, false);
+            return css + _js_bundle +  "<script type='text/javascript' src='" + cdn + "'></script>" + osmdCanvas + script + osmd_app_js;
+            });
+        } else {
+            var text = getLocalMusicXML(musicxml);
+            var script = util.htmlTag('script', {}, 'var mxl = \'' + text + '\';', false);
+            var osmdCanvas = util.htmlTag('div', {
+                id: 'osmdCanvas'
+            }, loding_box + _canvas, false);
+            return css + _js_bundle +  "<script type='text/javascript' src='" + cdn + "'></script>" + osmdCanvas + script + osmd_app_js;
+        }
+    } else {
+        console.log("Load Without Player!");
+
+        var css = util.htmlTag('link', {
+            href: 'https://cdn.jsdelivr.net/npm/hexo-musicxml-sheetmusic@0.0.5/osmd-c.css',
+            rel: 'stylesheet'
+        });
+
+        var _canvas = util.htmlTag('canvas', {id: "canvas", style: "display:none;"},"",false);
+
+        if(musicxml.indexOf("http") != -1) {
+            return result = getMusicXML(musicxml).then(text => {
+                var loadScript = 'var osmd = new opensheetmusicdisplay.OpenSheetMusicDisplay("osmdCanvas");';
+                loadScript += ('var mxl = \'' + text + '\';');
+                loadScript += 'osmd.load(mxl);';
+                loadScript += 'osmd.render();';
+                var script = util.htmlTag('script', {}, loadScript, false);
+
+                var osmdCanvas = util.htmlTag('div', {
+                    id: 'osmdCanvas'
+                }, _canvas, false);
+            return css + "<script type='text/javascript' src='" + cdn + "'></script>" + osmdCanvas + script;
+            });
+        } else {
+            var text = getLocalMusicXML(musicxml);
+            var loadScript = 'var osmd = new opensheetmusicdisplay.OpenSheetMusicDisplay("osmdCanvas");';
+            loadScript += ('var mxl = \'' + text + '\';');
+            loadScript += 'osmd.load(mxl);';
+            loadScript += 'osmd.render();';
+            var script = util.htmlTag('script', {}, loadScript, false);
+
+            var osmdCanvas = util.htmlTag('div', {
+                id: 'osmdCanvas'
+            }, _canvas, false);
+            return css + "<script type='text/javascript' src='" + cdn + "'></script>" + osmdCanvas + script;
+        }
+    }
+
+}, {
+    async: true
+});
+
+async function getMusicXML(href) {
+    return request(href).then(function (musicxml) {
+        musicxml = minifyXML(musicxml);
+        console.log("GET Internet MusicXML => SUCCESS!");
+        //console.log(musicxml.replace(/[\r\n]/g,""));
+        return musicxml.replace(/[\r\n]/g,"");
+    });
+}
+
+function getLocalMusicXML(path) {
+    if(fs.existsSync(path)) {
+        var musicxml = fs.readFileSync(path,'utf8') 
+        musicxml = minifyXML(musicxml);
+        console.log("GET Local MusicXML => SUCCESS!");
+        //console.log(musicxml.replace(/[\r\n]/g,""));
+        return musicxml.replace(/[\r\n]/g,"");
+    } else {
+        console.log("Load Failed.");
+        let error = "No File in Path " + path + " Found.";
+        throw error;
+        return;
+    }
+}
